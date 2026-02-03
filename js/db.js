10f06188b29c6466b1cdf72a656488f38daea32c
iamprince0909-max <iamprince0909@gmail.com>
2026-02-03 13:02:31 +0530

    db.js

--- js/db.js
+++ js/db.js
@@ -1,26 +1,5 @@
-let db=null;
+let DB = JSON.parse(localStorage.getItem("BAG_ERP_DB")) || { styles:{} };
 
-const request=indexedDB.open("BagERP_DB",1);
-
-request.onupgradeneeded=e=>{
-  db=e.target.result;
-  if(!db.objectStoreNames.contains("orders")){
-    db.createObjectStore("orders",{keyPath:"id",autoIncrement:true});
-  }
-};
-
-request.onsuccess=e=>{
-  db=e.target.result;
-  document.dispatchEvent(new Event("db-ready"));
-};
-
-function addOrder(o){
-  db.transaction("orders","readwrite").objectStore("orders").add(o);
-}
-function getOrders(cb){
-  const r=db.transaction("orders","readonly").objectStore("orders").getAll();
-  r.onsuccess=()=>cb(r.result);
-}
-function clearOrders(){
-  db.transaction("orders","readwrite").objectStore("orders").clear();
+function saveDB(){
+localStorage.setItem("BAG_ERP_DB", JSON.stringify(DB));
 }
